0de7ee82cbad8f08720a8cc4df1181c42927875c
Patrick Patel <patrickpatel96@gmail.com>
2021-10-19 12:27:33 -0700

    Add registration jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import $ from 'jquery';
 
 class Login extends React.Component {
   constructor(props) {
---
+++ client/src/components/Register.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import $ from 'jquery';
+
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      password2: '',
+    }
+  }
+
+  onChange({ target }) {
+    this.setState({ [target.name]: target.value });
+  }
+
+  login() {
+    $.ajax({
+      // 'url': 'http://localhost:1128/login',
+      'url': '/register',
+      'type': 'POST',
+      'context': this,
+      'data': this.state,
+      'success': function() {
+        console.log('success');
+      },
+      'error': function(error) {
+        console.log(error);
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <label for="email"><b>Email</b></label>
+          <input type="text" placeholder="Enter Email" name="email" value={this.state.email} onChange={this.onChange} required>
+
+          <label for="psw"><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" name="psw" value={this.state.password} onChange={this.onChange} required>
+
+          <label for="psw2"><b>Password</b></label>
+          <input type="password" placeholder="Confirm Password" name="psw2" value={this.state.password2} onChange={this.onChange} required>
+
+          <button type="submit" onSubmit={this.login.bind(this)} >Register</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Register;

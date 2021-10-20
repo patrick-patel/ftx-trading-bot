f8884c354650759a2fd5952713a34367e459b198
Patrick Patel <patrickpatel96@gmail.com>
2021-10-20 13:07:39 -0700

    more debugging login route

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -12,10 +12,12 @@ const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.secretOrKey || config.secretOrKey;
 module.exports = passport => {
+  console.log('using passport');
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
       User.findById(jwt_payload.id)
         .then(user => {
+          console.log('user from passport: ', user);
           if (user) {
             return done(null, user);
           }

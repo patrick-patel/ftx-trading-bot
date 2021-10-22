5bc1cd5d759cc6a1e85c23c02014a802d7479120
Patrick Patel <patrickpatel96@gmail.com>
2021-10-22 14:37:34 -0700

    Fix bug...again

--- client/src/components/Settings.jsx
+++ client/src/components/Settings.jsx
@@ -148,56 +148,32 @@ class Settings extends React.Component {
           <br>
           <p>{this.state.apiValue ? <label for="ETH/BTC"><b>ETH/BTC Pair</b></label> : null}</p>
           <div>
-            {this.state.apiValue ?
-
-                <input type="radio" name="ETH/BTC" value={this.state.["ETH/BTC"]} onChange={this.onChangeRadio.bind(this)} required></input>
-                : null
-            }
+            {this.state.apiValue ? <input type="radio" name="ETH/BTC" value={this.state.["ETH/BTC"]} onChange={this.onChangeRadio.bind(this)} required></input> : null}
           </div>
 
           <p>{this.state.apiValue ? <label for="LINK/BTC"><b>LINK/BTC Pair</b></label> : null}</p>
           <div>
-            {this.state.apiValue ?
-                <label for="LINK/BTC"><b>LINK/BTC Pair</b></label>
-                <input type="radio" name="LINK/BTC" value={this.state.["LINK/BTC"]} onChange={this.onChangeRadio.bind(this)} required></input>
-                : null
-            }
+            {this.state.apiValue ? <input type="radio" name="LINK/BTC" value={this.state.["LINK/BTC"]} onChange={this.onChangeRadio.bind(this)} required></input> : null}
           </div>
 
           <p>{this.state.apiValue ? <label for="MATIC/BTC"><b>MATIC/BTC Pair</b></label> : null}</p>
           <div>
-            {this.state.apiValue ?
-                <label for="MATIC/BTC"><b>MATIC/BTC Pair</b></label>
-                <input type="radio" name="MATIC/BTC" value={this.state.["MATIC/BTC"]} onChange={this.onChangeRadio.bind(this)} required></input>
-                : null
-            }
+            {this.state.apiValue ? <input type="radio" name="MATIC/BTC" value={this.state.["MATIC/BTC"]} onChange={this.onChangeRadio.bind(this)} required></input> : null}
           </div>
 
           <p>{this.state.apiValue ? <label for="SOL/BTC"><b>SOL/BTC Pair</b></label> : null}</p>
           <div>
-            {this.state.apiValue ?
-                <label for="SOL/BTC"><b>SOL/BTC Pair</b></label>
-                <input type="radio" name="SOL/BTC" value={this.state.["SOL/BTC"]} onChange={this.onChangeRadio.bind(this)} required></input>
-                : null
-            }
+            {this.state.apiValue ? <input type="radio" name="SOL/BTC" value={this.state.["SOL/BTC"]} onChange={this.onChangeRadio.bind(this)} required></input> : null}
           </div>
 
           <p>{this.state.apiValue ? <label for="SUSHI/BTC"><b>SUSHI/BTC Pair</b></label> : null}</p>
           <div>
-            {this.state.apiValue ?
-                <label for="SUSHI/BTC"><b>SUSHI/BTC Pair</b></label>
-                <input type="radio" name="SUSHI/BTC" value={this.state.["SUSHI/BTC"]} onChange={this.onChangeRadio.bind(this)} required></input>
-                : null
-            }
+            {this.state.apiValue ? <input type="radio" name="SUSHI/BTC" value={this.state.["SUSHI/BTC"]} onChange={this.onChangeRadio.bind(this)} required></input> : null}
           </div>
 
           <p>{this.state.apiValue ? <label for="UNI/BTC"><b>UNI/BTC Pair</b></label> : null}</p>
           <div>
-            {this.state.apiValue ?
-                <label for="UNI/BTC"><b>UNI/BTC Pair</b></label>
-                <input type="radio" name="UNI/BTC" value={this.state.["UNI/BTC"]} onChange={this.onChangeRadio.bind(this)} required></input>
-                : null
-            }
+            {this.state.apiValue ? <input type="radio" name="UNI/BTC" value={this.state.["UNI/BTC"]} onChange={this.onChangeRadio.bind(this)} required></input> : null}
           </div>
           <button type="submit" onClick={this.submitPairs.bind(this)}>Submit</button>
         </div>

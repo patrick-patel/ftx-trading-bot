5b267ba3e7cbb234911964e3206196df924e3f58
Patrick Patel <patrickpatel96@gmail.com>
2021-10-26 12:45:28 -0700

    refactor to get api comps to show

--- client/src/components/settings/API.jsx
+++ client/src/components/settings/API.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PairsForm from './PairsForm.jsx';
 
-const API = (props) => (
-  <h4>{props.api_key}</h4>
-  <PairsForm onChangeRadio={props.onChangeRadio} submitPairs={props.submitPairs} />
-  <p>--------------------------</p>
+const API = ({api_key}) => (
+  <div>
+    <h4>{api_key}</h4>
+    <PairsForm onChangeRadio={props.onChangeRadio} submitPairs={props.submitPairs} />
+    <p>--------------------------</p>
+  </div>
 )
 
 export default API;

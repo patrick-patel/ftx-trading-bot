f1960c924f4ed7a74259c355cb1b83da0f70522e
Patrick Patel <patrickpatel96@gmail.com>
2021-10-26 15:42:44 -0700

    Add return to render

--- client/src/components/settings/API.jsx
+++ client/src/components/settings/API.jsx
@@ -21,12 +21,14 @@ class API extends React.Component {
   }
 
   render() {
-    <div>
-      <h4>{this.props.api_key}</h4>
-      {this.state.edit ? <PairsForm onChangeRadio={this.props.onChangeRadio} submitPairs={this.props.submitPairs} /> : null}
-      <Button onClick={this.onEdit.bind(this)}>Edit Pairs</Button>
-      <p>--------------------------</p>
-    </div>
+    return (
+      <div>
+        <h4>{this.props.api_key}</h4>
+        {this.state.edit ? <PairsForm onChangeRadio={this.props.onChangeRadio} submitPairs={this.props.submitPairs} /> : null}
+        <Button onClick={this.onEdit.bind(this)}>Edit Pairs</Button>
+        <p>--------------------------</p>
+      </div>
+    )
   }
 }
 

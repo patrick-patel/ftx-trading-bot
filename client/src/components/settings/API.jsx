fe3de6ebdbeb8f49140064ef97b0faec8561f92a
Patrick Patel <patrickpatel96@gmail.com>
2021-10-26 15:26:38 -0700

    Add edit button and ternary for showing pairs

--- client/src/components/settings/API.jsx
+++ client/src/components/settings/API.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import PairsForm from './PairsForm.jsx';
 
-const API = (props) => (
-  <div>
-    <h4>{props.api_key}</h4>
-    <PairsForm onChangeRadio={props.onChangeRadio} submitPairs={props.submitPairs} />
-    <p>--------------------------</p>
-  </div>
-)
+import { ButtonGroup, Button } from 'react-bootstrap';
+
+
+class API extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      edit: false
+    }
+  }
+
+  onEdit() {
+    if (!this.state.edit) {
+      this.setState({ edit: true })
+    } else {
+      this.setState({ edit: false })
+    }
+  }
+
+  render() {
+    <div>
+      <h4>{props.api_key}</h4>
+      {this.state.edit ? <PairsForm onChangeRadio={props.onChangeRadio} submitPairs={props.submitPairs} /> : null}
+      <Button onClick={this.onEdit.bind(this)}>Edit Pairs</Button>
+      <p>--------------------------</p>
+    </div>
+  }
+}
 
 export default API;

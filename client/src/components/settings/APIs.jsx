d61e449e4178b1937e368d811278bb739ec6e09b
Patrick Patel <patrickpatel96@gmail.com>
2021-10-26 11:22:45 -0700

    add ternary to props keys

--- client/src/components/settings/APIs.jsx
+++ client/src/components/settings/APIs.jsx
@@ -5,16 +5,18 @@ const APIs = (props) => (
   <div>
     <h4>API Keys</h4>
     <ul>
-      {Object.keys(props.api_keys).map(key => {
-        <API api_key={props.api_keys[key]}
-           ethbtc={props["ETH/BTC"]}
-           linkbtc={props["LINK/BTC"]}
-           maticbtc={props["MATIC/BTC"]}
-           solbtc={props["SOL/BTC"]}
-           sushibtc={props["SUSHI/BTC"]}
-           unibtc={props["UNI/BTC"]}
+      {Object.keys(props.api_keys) ? Object.keys(props.api_keys).map(key => {
+        <API
+          api_key={props.api_keys[key]}
+          ethbtc={props["ETH/BTC"]}
+          linkbtc={props["LINK/BTC"]}
+          maticbtc={props["MATIC/BTC"]}
+          solbtc={props["SOL/BTC"]}
+          sushibtc={props["SUSHI/BTC"]}
+          unibtc={props["UNI/BTC"]}
         />
-      })}
+      })
+      : null}
     </ul>
   </div>
 

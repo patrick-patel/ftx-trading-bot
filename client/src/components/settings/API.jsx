92ea69eb21ef03f96af40727be657e2fb9639c4b
Patrick Patel <patrickpatel96@gmail.com>
2021-10-26 12:38:47 -0700

    Remove div on api comp

--- client/src/components/settings/API.jsx
+++ client/src/components/settings/API.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import PairsForm from './PairsForm.jsx';
 
 const API = (props) => (
-  <div>
-    <h4>{props.api_key}</h4>
-    <PairsForm onChangeRadio={props.onChangeRadio} submitPairs={props.submitPairs}/>
-    <p>--------------------------</p>
-  </div>
+  <h4>{props.api_key}</h4>
+  <PairsForm onChangeRadio={props.onChangeRadio} submitPairs={props.submitPairs} />
+  <p>--------------------------</p>
 )
 
 export default API;

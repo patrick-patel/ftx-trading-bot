5432a9bd6f84324555f2189939a1b8edffd7adb0
Patrick Patel <patrickpatel96@gmail.com>
2021-10-26 12:48:42 -0700

    got it!

--- client/src/components/settings/API.jsx
+++ client/src/components/settings/API.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import PairsForm from './PairsForm.jsx';
 
-const API = ({api_key}) => (
+const API = (props) => (
   <div>
-    <h4>{api_key}</h4>
+    <h4>{props.api_key}</h4>
     <PairsForm onChangeRadio={props.onChangeRadio} submitPairs={props.submitPairs} />
     <p>--------------------------</p>
   </div>

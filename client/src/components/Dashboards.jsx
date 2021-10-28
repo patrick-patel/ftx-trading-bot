f6b076774aef45201b4de2fb91098dc19966770b
Patrick Patel <patrickpatel96@gmail.com>
2021-10-28 14:18:37 -0700

    switch to render in index

--- client/src/components/Dashboards.jsx
+++ client/src/components/Dashboards.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Dashboard from './Dashboard.jsx'
 
-const Dashboards = (props) => (
+const Dashboards = ({ credentials }) => (
   <div>
     <h1>FTX Trading Bot</h1>
-    {props.credentials[0].api_key ? <ul>{props.credentials.map(credential => <Dashboard credential={credential} />)}</ul> : null}
+    {credentials[0].api_key ? <ul>{props.credentials.map(credential => <Dashboard credential={credential} />)}</ul> : null}
   </div>
 )
 

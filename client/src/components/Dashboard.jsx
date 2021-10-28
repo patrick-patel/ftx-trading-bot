75f5be229af22caa2e6156da64189810b872f342
Patrick Patel <patrickpatel96@gmail.com>
2021-10-28 14:08:11 -0700

    move ternary to each element

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -2,19 +2,17 @@ import React from 'react';
 
 const Dashboard = ({ credential }) => (
   <div>
-    {credential.api_key ?
     <div style={{color: 'white', backgroundColor: 'black', padding: '20px'}}>
-      <p>API Key: {credential.api_key}</p>
-      <p>Account Value USD:  {credential.total}</p>
-      <p>BTC: {credential.btc}</p>
-      <p>ETH: {credential.eth}</p>
-      <p>LINK: {credential.link}</p>
-      <p>MATIC: {credential.matic}</p>
-      <p>SOL: {credential.sol}</p>
-      <p>SUSHI: {credential.sushi}</p>
-      <p>UNI: {credential.uni}</p>
+    {credential.api_key ? <p>API Key: {credential.api_key}</p> : null}
+      {credential.api_key ? <p>Account Value USD:  {credential.total}</p> : null}
+      {credential.api_key ? <p>BTC: {credential.btc}</p> : null}
+      {credential.api_key ? <p>ETH: {credential.eth}</p> : null}
+      {credential.api_key ? <p>LINK: {credential.link}</p> : null}
+      {credential.api_key ? <p>MATIC: {credential.matic}</p> : null}
+      {credential.api_key ? <p>SOL: {credential.sol}</p> : null}
+      {credential.api_key ? <p>SUSHI: {credential.sushi}</p> : null}
+      {credential.api_key ? <p>UNI: {credential.uni}</p> : null}
     </div>
-    : null}
   </div>
 )
 

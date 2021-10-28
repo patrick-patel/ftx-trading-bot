68b82f6acd854a895b55c5b83f8bd14ff76af986
Patrick Patel <patrickpatel96@gmail.com>
2021-10-28 14:46:21 -0700

    remove props from dashboards

--- client/src/components/Dashboards.jsx
+++ client/src/components/Dashboards.jsx
@@ -4,7 +4,7 @@ import Dashboard from './Dashboard.jsx'
 const Dashboards = ({ credentials }) => (
   <div>
     <h1>FTX Trading Bot</h1>
-    {credentials.length > 0 ? <ul>{props.credentials.map(credential => <Dashboard credential={credential} />)}</ul> : null}
+    {credentials.length > 0 ? <ul>{credentials.map(credential => <Dashboard credential={credential} />)}</ul> : null}
   </div>
 )
 

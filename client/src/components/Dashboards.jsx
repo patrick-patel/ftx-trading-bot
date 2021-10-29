2710aeed4870dd80cd901988bae2c558b91f0a77
Patrick Patel <patrickpatel96@gmail.com>
2021-10-29 15:55:26 -0700

    minor ui adjustment

--- client/src/components/Dashboards.jsx
+++ client/src/components/Dashboards.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Dashboard from './Dashboard.jsx'
+import { Container } from 'react-bootstrap';
+
 
 const Dashboards = ({ credentials }) => (
-  <div>
+  <Container>
     <h2>Dashboard</h2>
     {credentials.length > 0 ? <ul>{credentials.map(credential => <Dashboard credential={credential} />)}</ul> : null}
-  </div>
+  </Container>
 )
 
 export default Dashboards;

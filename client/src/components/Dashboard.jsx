b6503f53ce23b4407181cc14bff8cc788fc2713c
Patrick Patel <patrickpatel96@gmail.com>
2021-11-04 10:24:22 -0700

    remove chartjs and add google chart

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { MDBContainer } from "mdbreact";
-import { Line } from "react-chartjs-2";
+import { Chart } from "react-google-charts";
 import { Col, Row } from 'react-bootstrap';
 
 
@@ -125,9 +124,36 @@ const Dashboard = ({ credential, data }) => (
         </Row>
       </Col>
       <Col className="border">
-        <MDBContainer>
-          <Line data={data} />
-        </MDBContainer>
+        <Chart
+          width={'600px'}
+          height={'400px'}
+          chartType="LineChart"
+          loader={<div>Loading Chart</div>}
+          data={[
+            ['x', 'dogs'],
+            [0, 0],
+            [1, 10],
+            [2, 23],
+            [3, 17],
+            [4, 18],
+            [5, 9],
+            [6, 11],
+            [7, 27],
+            [8, 33],
+            [9, 40],
+            [10, 32],
+            [11, 35],
+          ]}
+          options={{
+            hAxis: {
+              title: 'Time',
+            },
+            vAxis: {
+              title: 'Popularity',
+            },
+          }}
+          rootProps={{ 'data-testid': '1' }}
+        />
       </Col>
     </Row>
     <hr></hr>

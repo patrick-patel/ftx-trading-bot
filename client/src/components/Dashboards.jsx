3b4a7145f65eabf5846ae0b11a5ffc065e0dc42d
Patrick Patel <patrickpatel96@gmail.com>
2021-11-04 09:21:14 -0700

    fix bug in dashboards ternary

--- client/src/components/Dashboards.jsx
+++ client/src/components/Dashboards.jsx
@@ -6,7 +6,7 @@ import { Container } from 'react-bootstrap';
 const Dashboards = ({ credentials }) => (
   <Container style={{padding: '20px'}}>
     <h5 className="display-5">Dashboard</h5>
-    {credentials.length > 0 ? credentials.map(credential => <Dashboard credential={credential} />)} : null}
+    {credentials.length > 0 ? credentials.map(credential => <Dashboard credential={credential} />) : null}
   </Container>
 )
 

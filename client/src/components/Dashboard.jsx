61dde6a60389b7a41d56bd5e6263ea451c74cc7e
Patrick Patel <patrickpatel96@gmail.com>
2021-11-04 11:17:13 -0700

    testing with chart

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -31,87 +31,87 @@ const Dashboard = ({ credential, data }) => (
         <Row>
           <Col className="border-end border-top">
             <div>
-              {credential.isSubscribedTo["ETH/BTC"]["1hr"] ? <p className="lead">ETH/BTC 1hr</p> : null}
-              {credential.isSubscribedTo["ETH/BTC"]["2hr"] ? <p className="lead">ETH/BTC 2hr</p> : null}
-              {credential.isSubscribedTo["ETH/BTC"]["4hr"] ? <p className="lead">ETH/BTC 4hr</p> : null}
-              {credential.isSubscribedTo["ETH/BTC"]["6hr"] ? <p className="lead">ETH/BTC 6hr</p> : null}
-              {credential.isSubscribedTo["ETH/BTC"]["12hr"] ? <p className="lead">ETH/BTC 12hr</p> : null}
+              {credential.isSubscribedTo["ETH/BTC"]["1hr"] ? <p className="lead">BTC 1hr</p> : null}
+              {credential.isSubscribedTo["ETH/BTC"]["2hr"] ? <p className="lead">BTC 2hr</p> : null}
+              {credential.isSubscribedTo["ETH/BTC"]["4hr"] ? <p className="lead">BTC 4hr</p> : null}
+              {credential.isSubscribedTo["ETH/BTC"]["6hr"] ? <p className="lead">BTC 6hr</p> : null}
+              {credential.isSubscribedTo["ETH/BTC"]["12hr"] ? <p className="lead">BTC 12hr</p> : null}
 
-              {credential.isSubscribedTo["ETH/USD"]["1hr"] ? <p className="lead">ETH/USD 1hr</p> : null}
-              {credential.isSubscribedTo["ETH/USD"]["2hr"] ? <p className="lead">ETH/USD 2hr</p> : null}
-              {credential.isSubscribedTo["ETH/USD"]["4hr"] ? <p className="lead">ETH/USD 4hr</p> : null}
-              {credential.isSubscribedTo["ETH/USD"]["6hr"] ? <p className="lead">ETH/USD 6hr</p> : null}
-              {credential.isSubscribedTo["ETH/USD"]["12hr"] ? <p className="lead">ETH/USD 12hr</p> : null}
+              {credential.isSubscribedTo["ETH/USD"]["1hr"] ? <p className="lead">USD 1hr</p> : null}
+              {credential.isSubscribedTo["ETH/USD"]["2hr"] ? <p className="lead">USD 2hr</p> : null}
+              {credential.isSubscribedTo["ETH/USD"]["4hr"] ? <p className="lead">USD 4hr</p> : null}
+              {credential.isSubscribedTo["ETH/USD"]["6hr"] ? <p className="lead">USD 6hr</p> : null}
+              {credential.isSubscribedTo["ETH/USD"]["12hr"] ? <p className="lead">USD 12hr</p> : null}
             </div>
           </Col>
           <Col className="border-end border-top">
             <div>
-              {credential.isSubscribedTo["LINK/BTC"]["1hr"] ? <p className="lead">LINK/BTC 1hr</p> : null}
-              {credential.isSubscribedTo["LINK/BTC"]["2hr"] ? <p className="lead">LINK/BTC 2hr</p> : null}
-              {credential.isSubscribedTo["LINK/BTC"]["4hr"] ? <p className="lead">LINK/BTC 4hr</p> : null}
-              {credential.isSubscribedTo["LINK/BTC"]["6hr"] ? <p className="lead">LINK/BTC 6hr</p> : null}
-              {credential.isSubscribedTo["LINK/BTC"]["12hr"] ? <p className="lead">LINK/BTC 12hr</p> : null}
+              {credential.isSubscribedTo["LINK/BTC"]["1hr"] ? <p className="lead">BTC 1hr</p> : null}
+              {credential.isSubscribedTo["LINK/BTC"]["2hr"] ? <p className="lead">BTC 2hr</p> : null}
+              {credential.isSubscribedTo["LINK/BTC"]["4hr"] ? <p className="lead">BTC 4hr</p> : null}
+              {credential.isSubscribedTo["LINK/BTC"]["6hr"] ? <p className="lead">BTC 6hr</p> : null}
+              {credential.isSubscribedTo["LINK/BTC"]["12hr"] ? <p className="lead">BTC 12hr</p> : null}
 
-              {credential.isSubscribedTo["LINK/USD"]["1hr"] ? <p className="lead">LINK/USD 1hr</p> : null}
-              {credential.isSubscribedTo["LINK/USD"]["2hr"] ? <p className="lead">LINK/USD 2hr</p> : null}
-              {credential.isSubscribedTo["LINK/USD"]["4hr"] ? <p className="lead">LINK/USD 4hr</p> : null}
-              {credential.isSubscribedTo["LINK/USD"]["6hr"] ? <p className="lead">LINK/USD 6hr</p> : null}
-              {credential.isSubscribedTo["LINK/USD"]["12hr"] ? <p className="lead">LINK/USD 12hr</p> : null}
+              {credential.isSubscribedTo["LINK/USD"]["1hr"] ? <p className="lead">USD 1hr</p> : null}
+              {credential.isSubscribedTo["LINK/USD"]["2hr"] ? <p className="lead">USD 2hr</p> : null}
+              {credential.isSubscribedTo["LINK/USD"]["4hr"] ? <p className="lead">USD 4hr</p> : null}
+              {credential.isSubscribedTo["LINK/USD"]["6hr"] ? <p className="lead">USD 6hr</p> : null}
+              {credential.isSubscribedTo["LINK/USD"]["12hr"] ? <p className="lead">USD 12hr</p> : null}
             </div>
           </Col>
           <Col className="border-end border-top">
             <div>
-              {credential.isSubscribedTo["MATIC/BTC"]["1hr"] ? <p className="lead">MATIC/BTC 1hr</p> : null}
-              {credential.isSubscribedTo["MATIC/BTC"]["2hr"] ? <p className="lead">MATIC/BTC 2hr</p> : null}
-              {credential.isSubscribedTo["MATIC/BTC"]["4hr"] ? <p className="lead">MATIC/BTC 4hr</p> : null}
-              {credential.isSubscribedTo["MATIC/BTC"]["6hr"] ? <p className="lead">MATIC/BTC 6hr</p> : null}
-              {credential.isSubscribedTo["MATIC/BTC"]["12hr"] ? <p className="lead">MATIC/BTC 12hr</p> : null}
+              {credential.isSubscribedTo["MATIC/BTC"]["1hr"] ? <p className="lead">BTC 1hr</p> : null}
+              {credential.isSubscribedTo["MATIC/BTC"]["2hr"] ? <p className="lead">BTC 2hr</p> : null}
+              {credential.isSubscribedTo["MATIC/BTC"]["4hr"] ? <p className="lead">BTC 4hr</p> : null}
+              {credential.isSubscribedTo["MATIC/BTC"]["6hr"] ? <p className="lead">BTC 6hr</p> : null}
+              {credential.isSubscribedTo["MATIC/BTC"]["12hr"] ? <p className="lead">BTC 12hr</p> : null}
 
-              {credential.isSubscribedTo["MATIC/USD"]["1hr"] ? <p className="lead">MATIC/USD 1hr</p> : null}
-              {credential.isSubscribedTo["MATIC/USD"]["2hr"] ? <p className="lead">MATIC/USD 2hr</p> : null}
-              {credential.isSubscribedTo["MATIC/USD"]["4hr"] ? <p className="lead">MATIC/USD 4hr</p> : null}
-              {credential.isSubscribedTo["MATIC/USD"]["6hr"] ? <p className="lead">MATIC/USD 6hr</p> : null}
-              {credential.isSubscribedTo["MATIC/USD"]["12hr"] ? <p className="lead">MATIC/USD 12hr</p> : null}
+              {credential.isSubscribedTo["MATIC/USD"]["1hr"] ? <p className="lead">USD 1hr</p> : null}
+              {credential.isSubscribedTo["MATIC/USD"]["2hr"] ? <p className="lead">USD 2hr</p> : null}
+              {credential.isSubscribedTo["MATIC/USD"]["4hr"] ? <p className="lead">USD 4hr</p> : null}
+              {credential.isSubscribedTo["MATIC/USD"]["6hr"] ? <p className="lead">USD 6hr</p> : null}
+              {credential.isSubscribedTo["MATIC/USD"]["12hr"] ? <p className="lead">USD 12hr</p> : null}
             </div>
           </Col>
           <Col className="border-end border-top">
             <div>
-              {credential.isSubscribedTo["SOL/BTC"]["1hr"] ? <p className="lead">SOL/BTC 1hr</p> : null}
-              {credential.isSubscribedTo["SOL/BTC"]["2hr"] ? <p className="lead">SOL/BTC 2hr</p> : null}
-              {credential.isSubscribedTo["SOL/BTC"]["4hr"] ? <p className="lead">SOL/BTC 4hr</p> : null}
-              {credential.isSubscribedTo["SOL/BTC"]["6hr"] ? <p className="lead">SOL/BTC 6hr</p> : null}
-              {credential.isSubscribedTo["SOL/BTC"]["12hr"] ? <p className="lead">SOL/BTC 12hr</p> : null}
+              {credential.isSubscribedTo["SOL/BTC"]["1hr"] ? <p className="lead">BTC 1hr</p> : null}
+              {credential.isSubscribedTo["SOL/BTC"]["2hr"] ? <p className="lead">BTC 2hr</p> : null}
+              {credential.isSubscribedTo["SOL/BTC"]["4hr"] ? <p className="lead">BTC 4hr</p> : null}
+              {credential.isSubscribedTo["SOL/BTC"]["6hr"] ? <p className="lead">BTC 6hr</p> : null}
+              {credential.isSubscribedTo["SOL/BTC"]["12hr"] ? <p className="lead">BTC 12hr</p> : null}
 
-              {credential.isSubscribedTo["SOL/USD"]["1hr"] ? <p className="lead">SOL/USD 1hr</p> : null}
-              {credential.isSubscribedTo["SOL/USD"]["2hr"] ? <p className="lead">SOL/USD 2hr</p> : null}
-              {credential.isSubscribedTo["SOL/USD"]["4hr"] ? <p className="lead">SOL/USD 4hr</p> : null}
-              {credential.isSubscribedTo["SOL/USD"]["6hr"] ? <p className="lead">SOL/USD 6hr</p> : null}
-              {credential.isSubscribedTo["SOL/USD"]["12hr"] ? <p className="lead">SOL/USD 12hr</p> : null}
+              {credential.isSubscribedTo["SOL/USD"]["1hr"] ? <p className="lead">USD 1hr</p> : null}
+              {credential.isSubscribedTo["SOL/USD"]["2hr"] ? <p className="lead">USD 2hr</p> : null}
+              {credential.isSubscribedTo["SOL/USD"]["4hr"] ? <p className="lead">USD 4hr</p> : null}
+              {credential.isSubscribedTo["SOL/USD"]["6hr"] ? <p className="lead">USD 6hr</p> : null}
+              {credential.isSubscribedTo["SOL/USD"]["12hr"] ? <p className="lead">USD 12hr</p> : null}
             </div>
           </Col>
           <Col className="border-top">
             <div>
-              {credential.isSubscribedTo["UNI/BTC"]["1hr"] ? <p className="lead">UNI/BTC 1hr</p> : null}
-              {credential.isSubscribedTo["UNI/BTC"]["2hr"] ? <p className="lead">UNI/BTC 2hr</p> : null}
-              {credential.isSubscribedTo["UNI/BTC"]["4hr"] ? <p className="lead">UNI/BTC 4hr</p> : null}
-              {credential.isSubscribedTo["UNI/BTC"]["6hr"] ? <p className="lead">UNI/BTC 6hr</p> : null}
-              {credential.isSubscribedTo["UNI/BTC"]["12hr"] ? <p className="lead">UNI/BTC 12hr</p> : null}
+              {credential.isSubscribedTo["UNI/BTC"]["1hr"] ? <p className="lead">BTC 1hr</p> : null}
+              {credential.isSubscribedTo["UNI/BTC"]["2hr"] ? <p className="lead">BTC 2hr</p> : null}
+              {credential.isSubscribedTo["UNI/BTC"]["4hr"] ? <p className="lead">BTC 4hr</p> : null}
+              {credential.isSubscribedTo["UNI/BTC"]["6hr"] ? <p className="lead">BTC 6hr</p> : null}
+              {credential.isSubscribedTo["UNI/BTC"]["12hr"] ? <p className="lead">BTC 12hr</p> : null}
 
-              {credential.isSubscribedTo["UNI/USD"]["1hr"] ? <p className="lead">UNI/USD 1hr</p> : null}
-              {credential.isSubscribedTo["UNI/USD"]["2hr"] ? <p className="lead">UNI/USD 2hr</p> : null}
-              {credential.isSubscribedTo["UNI/USD"]["4hr"] ? <p className="lead">UNI/USD 4hr</p> : null}
-              {credential.isSubscribedTo["UNI/USD"]["6hr"] ? <p className="lead">UNI/USD 6hr</p> : null}
-              {credential.isSubscribedTo["UNI/USD"]["12hr"] ? <p className="lead">UNI/USD 12hr</p> : null}
+              {credential.isSubscribedTo["UNI/USD"]["1hr"] ? <p className="lead">USD 1hr</p> : null}
+              {credential.isSubscribedTo["UNI/USD"]["2hr"] ? <p className="lead">USD 2hr</p> : null}
+              {credential.isSubscribedTo["UNI/USD"]["4hr"] ? <p className="lead">USD 4hr</p> : null}
+              {credential.isSubscribedTo["UNI/USD"]["6hr"] ? <p className="lead">USD 6hr</p> : null}
+              {credential.isSubscribedTo["UNI/USD"]["12hr"] ? <p className="lead">USD 12hr</p> : null}
             </div>
           </Col>
         </Row>
         <Row>
-          <Col className="border-end border-top">
+          <Col className="border-end">
             <div>
               <p className="lead">BTC: {credential["BTC"]}</p>
             </div>
           </Col>
-          <Col className="border-top">
+          <Col>
             <div>
               <p className="lead">USD: {credential["USD"]}</p>
             </div>
@@ -130,8 +130,6 @@ const Dashboard = ({ credential, data }) => (
           chartType="LineChart"
           loader={<div>Loading Chart</div>}
           data={[
-            ['x', 'dogs'],
-            [0, 0],
             [1, 10],
             [2, 23],
             [3, 17],
@@ -146,10 +144,10 @@ const Dashboard = ({ credential, data }) => (
           ]}
           options={{
             hAxis: {
-              title: 'Time',
+              title: 'Week',
             },
             vAxis: {
-              title: 'Popularity',
+              title: 'USD',
             },
           }}
           rootProps={{ 'data-testid': '1' }}

ce9e460269765abeb8542f125c76668d48d6ffeb
Patrick Patel <patrickpatel96@gmail.com>
2021-11-04 14:59:11 -0700

    change settings link to full site address

--- client/src/components/Dashboards.jsx
+++ client/src/components/Dashboards.jsx
@@ -6,7 +6,7 @@ import { Container } from 'react-bootstrap';
 const Dashboards = ({ credentials }) => (
   <Container fluid style={{padding: '20px'}}>
     <h5 className="display-5">Dashboard</h5>
-    {credentials.length > 0 ? credentials.map(credential => <Dashboard credential={credential}/>) : <p className="lead">No subaccounts linked, go to the <a href="/settings">settings</a> page to add them</p>}
+    {credentials.length > 0 ? credentials.map(credential => <Dashboard credential={credential}/>) : <p className="lead">No subaccounts linked, go to the <a href="https://ftx-trading-bot.herokuapp.com//settings">settings</a> page to add them</p>}
   </Container>
 )
 

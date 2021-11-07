c9fed0b963d2bae810b379d693e47c8e466f48b0
Patrick Patel <patrickpatel96@gmail.com>
2021-11-06 17:10:49 -0700

    remove pong messages

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -147,7 +147,7 @@ const Dashboard = ({ credential }) => (
           ]}
           options={{
             hAxis: {
-              title: 'Hour',
+              title: 'Day',
             },
             vAxis: {
               title: 'USD',

46ae761bfb5d6c6da53817b9f8386f0ce8bb8e9a
Patrick Patel <patrickpatel96@gmail.com>
2021-11-10 18:46:54 -0800

    retry build

--- client/src/components/settings/PairsForm.jsx
+++ client/src/components/settings/PairsForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Col, ButtonGroup, Button, Row} from 'react-bootstrap';
 
+
 const PairsForm = (props) => (
   <div>
     <p><label for="ETH"><b>ETH Trading Set To: {props.state["ETH/BTC"] === "off" ? "USD" props.state["ETH/USD"] : props.state["ETH/USD"] === "off" ? "BTC" props.state["ETH/BTC"] : "off"}</b></label></p>

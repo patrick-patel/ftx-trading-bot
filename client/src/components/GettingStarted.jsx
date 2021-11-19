6ca9bfecd2faa7f54e450388422150e1aa599745
Patrick Patel <patrickpatel96@gmail.com>
2021-11-19 14:52:51 -0800

    fix pictures loading

--- client/src/components/GettingStarted.jsx
+++ client/src/components/GettingStarted.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { Redirect } from 'react-router';
 import $ from 'jquery';
 import step1 from "../media/step1.png";
+import step11 from "../media/step1-1.png";
+import step2 from "../media/step2.png";
+import step3 from "../media/step3.png";
+import step4 from "../media/step4.png";
+import step5 from "../media/step5.png";
+
 
 import { Container, Image, Row } from 'react-bootstrap';
 
@@ -26,27 +32,27 @@ class GettingStarted extends React.Component {
           <Row>
             <p className="lead">Step 1: Create a subaccount on FTX</p>
             <Image src={step1} />
-            <Image src={require("../media/step1-1.png")} fluid />
+            <Image src={step11} />
             <p className="fw-light fst-italic">You can setup multiple subaccounts to run different trading pairs or time frames.</p>
           </Row>
           <Row>
             <p className="lead">Step 2: Create an API Key</p>
-            <Image src={require("../media/step2.png")} fluid />
+            <Image src={step2} />
             <p className="fw-light fst-italic">Only create one api key per subaccount you setup with this bot.</p>
           </Row>
           <Row>
             <p className="lead">Step 3: Enter API Key under the settings tab</p>
-            <Image src={require("../media/step3.png")} fluid />
+            <Image src={step3} />
             <p className="fw-light fst-italic">If you are using FTX US make sure to check next to "FTX US".</p>
           </Row>
           <Row>
             <p className="lead">Step 4: Define your trading pairs</p>
-            <Image src={require("../media/step4.png")} fluid />
+            <Image src={step4} />
             <p className="fw-light fst-italic">You can setup the bot to trade as many or as few coins as you'd like, but must choose between USD or BTC for each coin.</p>
           </Row>
           <Row>
             <p className="lead">Step 5: Fund your subaccount with USD, BTC or Both</p>
-            <Image src={require("../media/step5.png")} fluid />
+            <Image src={step5} />
             <p className="fw-light fst-italic">Make sure to add the appropriate base asset depending on the pairs you subscribed to.</p>
           </Row>
           <Row>
